e196092945d26837716b6bbe71ac5dcf96c2bcfa
Marco Yap <marcochristian@mail.ugm.ac.id>
2026-09-20 22:24:39 +0700

    feat: add button

---
+++ script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('button')
+button.addEventListener('click', pause)
+
+function pause() {
+    document.getElementById('button').innerHTML = "Start"
+    document.getElementById('button').style.backgroundColor = "#4169E1"
+    document.querySelector('.loading').style.animationPlayState = "paused"
+
+    button.removeEventListener('click', pause)
+    button.addEventListener('click', start)
+}
+
+function start() {
+    document.getElementById('button').innerHTML = "Pause"
+    document.getElementById('button').style.backgroundColor = "plum"
+    document.querySelector('.loading').style.animationPlayState = "running"
+
+    button.removeEventListener('click', start)
+    button.addEventListener('click', pause)
+}
+
